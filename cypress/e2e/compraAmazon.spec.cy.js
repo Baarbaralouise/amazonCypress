3e4a30b2f9f8fc70270b5697f78fe7eb5b1530f0
BarbaraL <bfonseal@emeal.nttdata.com>
2022-11-07 16:49:01 -0300

    Adicionando o Assert ao código

--- cypress/e2e/compraAmazon.spec.cy.js
+++ cypress/e2e/compraAmazon.spec.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 
-describe('Adicionar produtos ao carrinho - Verificar preço final', () => {
+describe('Adicionar produto ao carrinho - Verificar se o carrinho está vazio', () => {
 
     beforeEach(() => {
         cy.visit('https://www.amazon.com.br')
@@ -13,21 +13,22 @@ describe('Adicionar produtos ao carrinho - Verificar preço final', () => {
         cy.loginAmazon();
     })
 
-    it('Deve adicionar múltiplos produtos ao carrinho', () => {
+    it('Deve adicionar um produto e validar se tem produto no carrinho', () => {
         cy.get('#twotabsearchtextbox').type('kindle{enter}', {force:true})
         cy.wait(6000)
         cy.get('[data-asin="B07FQK1TS9"] > .sg-col-inner > .s-widget-container > .s-card-container > .a-spacing-base > .s-product-image-container > .rush-component > .a-link-normal > .a-section > .s-image', {force:true}).click()
         cy.wait(6000)
         cy.get('#add-to-cart-button', {force:true}).click()
         cy.wait(6000)
-       cy.get('.a-button-close > .a-icon').click()
+        cy.get('.a-button-close > .a-icon').click()
         //cy.get('.a-button-close')
-       /cy.wait(6000)
+        cy.wait(6000)
         cy.get('input[name="proceedToRetailCheckout"]').click()
-        //cy.get('#ap_email').type(`${Cypress.env('usuario.amazon')}`)
-       // cy.get('#continue').click()
-        //.get('#ap_password').type(`${Cypress.env('senha.amazon')}`)
-        //.get('#signInSubmit').click();
+        cy.get('#ap_email').type(`${Cypress.env('usuario.amazon')}`) 
+        cy.get('#continue').click() 
+        cy.get('#ap_password').type(`${Cypress.env('senha.amazon')}`)
+        cy.get('#signInSubmit').click();
+        cy.get('.a-color-link').should('be.visible');
        
         
     })
